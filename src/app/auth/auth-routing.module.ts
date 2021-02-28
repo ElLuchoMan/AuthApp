1a047e5461ac014824a32716a0b91e876c12c190
ElLuchoMan <luchoman96@gmail.com>
2021-02-28 15:24:30 -0500

    Agregadas rutas y lazyload

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainComponent, children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'registro', component: RegisterComponent },
+      { path: '**', redirectTo: 'login' },
+    ]
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
