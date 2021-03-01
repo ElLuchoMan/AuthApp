cf82a80de10db8279f0c35d492a58e8b407d8bc7
ElLuchoMan <luchoman96@gmail.com>
2021-02-28 20:04:13 -0500

    Se implementa guard y se mantiene sesión en local storage

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { AuthResponse, Usuario } from '../interfaces/interfaces';
 import { map, catchError, tap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +21,7 @@ export class AuthService {
     return this.http.post<AuthResponse>(url, body).pipe(
       tap(resp => {
         if (resp.ok) {
-          localStorage.setItem('token',resp.token!);
+          localStorage.setItem('token', resp.token!);
           this._usuario = {
             name: resp.name!,
             uid: resp.uid!,
@@ -31,9 +31,18 @@ export class AuthService {
       map(resp => resp.ok), catchError(err => of(err.error.msg))
     );
   }
-  validarToken(){
+  validarToken(): Observable<boolean> {
     const url = `${this.baseUrl}/auth/renew`;
-    const headers = new HttpHeaders().set('x-token', localStorage.getItem('token')||'')
-    return this.http.get(url,{headers});
+    const headers = new HttpHeaders().set('x-token', localStorage.getItem('token') || '')
+
+    return this.http.get<AuthResponse>(url, { headers }).pipe(map(resp => {
+      localStorage.setItem('token', resp.token!);
+      this._usuario = {
+        name: resp.name!,
+        uid: resp.uid!,
+      }
+      return resp.ok;
+    }), catchError(err => of(false))
+    );
   }
 }
